f5b7917f262139694fad6985026d00a3e373f12e
Daen <g.dani325@gmail.com>
2019-01-20 17:05:53 +0100

    Part 2

--- two/main.cpp
+++ two/main.cpp
@@ -2,6 +2,32 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
+
+std::string produce_id(const std::vector<std::string>& input) {
+    std::vector<std::string> compare_with(input);
+
+    int diff_index;
+    for (const auto& lhs : input) {
+        compare_with.erase(std::find(compare_with.begin(), compare_with.end(), lhs));
+        for (const auto& rhs : compare_with) {
+            // assuming all box ids have the same length
+            int diff_characters = 0;
+            for (int i = 0; i < lhs.size(); i++) {
+                if (lhs[i] != rhs[i]) {
+                    diff_characters++;
+                    diff_index = i;
+                }
+            }
+            if (diff_characters == 1) {
+                std::string box_id(lhs);
+                box_id.erase(box_id.begin()+diff_index);
+                return box_id;
+            }
+        }
+    }
+    return std::string("");
+}
 
 int main() {
 
@@ -10,36 +36,12 @@ int main() {
     for (std::string line; std::getline(std::cin, line);) {
         input.push_back(line);
     }
-    
-    // process
-    int twos = 0;
-    int threes = 0;
-    for (const std::string& line : input) {
-        std::map<char, int> character_counts;
-        for (const char& c : line) {
-            if (character_counts.find(c) != character_counts.end()) { // key exists in map
-                character_counts[c]++;
-            } else {
-                character_counts.insert(std::make_pair(c, 1));
-            }
-        }
 
-        for (const auto& pair : character_counts) {
-            if (pair.second == 2) {
-                twos++;
-                break;
-            }
-        }
-        for (const auto& pair : character_counts) {
-            if (pair.second == 3) {
-                threes++;
-                break;
-            }
-        }
-    }
+    // process
+    std::string box_id = produce_id(input);
 
     // evaluate
-    std::cout << twos * threes << std::endl;
+    std::cout << box_id << std::endl;
 
     return 0;
 }
