40ebe5a86877fa14da8193a5e4b5d1637b9f9e4d
Daen <g.dani325@gmail.com>
2019-01-21 15:44:08 +0100

    Part 2

--- day-5/main.cpp
+++ day-5/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 std::string reduce(const std::string& input) {
     std::string polymer(input);
@@ -21,13 +22,21 @@ int main() {
     // read input
     std::istreambuf_iterator<char> begin(std::cin), end;
     std::string input(begin, end);
+    input.pop_back(); // remove endline character
 
     // process
-    std::string polymer = reduce(input);
-    polymer.pop_back(); // remove endline character
+    int minimum_size = input.size(); // set minimum to the highest possible at first
+    for (char c = 'a'; c <= 'z'; c++) {
+        std::string new_polymer(input);
+        new_polymer.erase(std::remove(new_polymer.begin(), new_polymer.end(), c), new_polymer.end());
+        new_polymer.erase(std::remove(new_polymer.begin(), new_polymer.end(), toupper(c)), new_polymer.end());
+        new_polymer = reduce(new_polymer);
+        minimum_size = new_polymer.size() < minimum_size ? new_polymer.size() : minimum_size;
+        std::cout << c << ": " << new_polymer.size() << std::endl; // see the progress
+    }
 
     // evaluate
-    std::cout << polymer.size() << std::endl;
+    std::cout << std::endl << "Minimum: " << minimum_size << std::endl;
 
     return 0;
 }
