b47bbb45d529b85c017088d9c7d9a15c8ac26714
Daniel Gal <denxyzw@gmail.com>
2022-12-02 23:00:23 +0100

    day 1 complete

--- 2022/src/index.ts
+++ 2022/src/index.ts
@@ -10,23 +10,29 @@ type State = { elves: Elf[] };
 const useStore = create<State>()(immer(() => ({ elves: [] })));
 
 const set = useStore.setState;
+const get = useStore.getState;
 
-const addElf = (elf: Elf) => {
+const addElf = (elf: Elf = []) => {
   set((state) => {
     state.elves.push(elf);
   });
 };
 
+const addSnack = (elfIndex: number, snack: number) => {
+  set((state) => {
+    state.elves[elfIndex].push(snack);
+  });
+};
+
 const initialize = (file: fs.PathLike) => {
   const input = new LineByLine(file);
-  let elf: Elf = [];
+  addElf();
   for (let line = input.next(); line; line = input.next()) {
     if (line.toString() === "") {
-      addElf(elf);
-      elf = [];
-    } else {
-      elf.push(Number(line.toString()));
+      addElf();
+      continue;
     }
+    addSnack(get().elves.length - 1, Number(line.toString()));
   }
 };
 
@@ -35,7 +41,15 @@ const selectSums = (state: State) =>
 
 const selectMax = (state: State) => Math.max(...selectSums(state));
 
+const selectSortedSums = (state: State) =>
+  lodash.sortBy(selectSums(state)).reverse();
+
+const selectSumOfTopThreeSums = (state: State) => {
+  const [first, second, third] = selectSortedSums(state);
+  return lodash.sum([first, second, third]);
+};
+
 // main
 
 initialize("public/input.txt");
-console.log(selectMax(useStore.getState()));
+console.log(selectSumOfTopThreeSums(useStore.getState()));
